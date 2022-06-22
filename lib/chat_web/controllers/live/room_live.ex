13fa1c4443253ff82e03047211c735361a3a68ca
Arooj Ahmad <arooj@Imrans-MacBook-Pro.local>
2022-06-22 10:33:38 -0500

    updating chat

--- lib/chat_web/controllers/live/room_live.ex
+++ lib/chat_web/controllers/live/room_live.ex
@@ -4,8 +4,22 @@ defmodule ChatWeb.RoomLive do
 
   @impl true
   def mount(%{"id" => room_id}, _session, socket) do
-    {:ok, socket}
+    topic = "room:" <> room_id
+    ChatWeb.Endpoint.subscribe(topic)
+    {:ok, assign(socket, room_id: room_id, topic: topic, messages: ["X joined the chat."])}
   end
 
+  @impl true
+  def handle_event("submit_message", %{"chat" => %{"message" => message}}, socket) do
+    Logger.info(message: message)
+    ChatWeb.Endpoint.broadcast(socket.assigns.topic, "new-message", message)
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_info(%{event: "new-message", payload: message}, socket) do
+    Logger.info(payload: message)
+    {:noreply, assign(socket, messages: socket.assigns.messages ++ [message])}
+  end
 
 end
